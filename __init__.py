b4f044c6db5156d5e1a838ca5515054768fcee2c
DJI28 <diogodomingues04@ua.pt>
2025-04-14 19:31:36 +0100

    plugin init

--- __init__.py
+++ __init__.py
@@ -2,20 +2,35 @@ from utils.api import Router
 from .router import router
 from .schemas.speaker_component import SpeakerComponent
 from services.component_registry import ComponentRegistry
+from services.ui.plugin_settings import create_plugin_setting, delete_plugin_setting_by_title
+from schemas.plugin_setting import PluginSetting
 import logging
 
 logger = logging.getLogger("coffeebreak.plugins.speaker")
 
+PLUGIN_TITLE = "Speakers Presentation"
+PLUGIN_DESCRIPTION = "A plugin for presenting speakers in a conference or event setting."
 
-def register_plugin():
+async def register_plugin():
     ComponentRegistry.register_component(SpeakerComponent)
     logger.debug("Speaker presentation plugin registered.")
+
+    setting = PluginSetting(
+        title=PLUGIN_TITLE,
+        description=PLUGIN_DESCRIPTION,
+        inputs=[]
+    )
+    await create_plugin_setting(setting)
+
     return router
 
-
-def unregister_plugin():
+async def unregister_plugin():
     ComponentRegistry.unregister_component("SpeakerComponent")
+    await delete_plugin_setting_by_title(PLUGIN_TITLE)
     logger.debug("Speaker presentation plugin unregistered.")
 
 REGISTER = register_plugin
 UNREGISTER = unregister_plugin
+
+SETTINGS = {}
+DESCRIPTION = PLUGIN_DESCRIPTION
