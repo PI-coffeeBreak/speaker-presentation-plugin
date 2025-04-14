38df58bed5df769325ea942ebf4c352c9c83f623
DJI28 <diogodomingues04@ua.pt>
2025-04-14 16:33:16 +0100

    component update

--- schemas/speaker_component.py
+++ schemas/speaker_component.py
@@ -1,8 +1,16 @@
-from pydantic import BaseModel, Field
 from schemas.ui.page import BaseComponentSchema
+from schemas.ui.components.text import Text
+from schemas.ui.components.title import Title
+from schemas.ui.components.image import Image
+from typing import Optional, Dict
+from pydantic import Field
 
 
 class SpeakerComponent(BaseComponentSchema):
-    name: str = Field(..., description="Speaker name")
-    description: str = Field(..., description="Speaker description")
-    image: str = Field(..., description="Speaker image URL")
+    title: Title = Field(..., description="Name/title of the speaker")
+    description: Text = Field(..., description="Speaker biography or description")
+    image: Image = Field(..., description="Speaker image")
+    contacts: Optional[Dict[str, str]] = Field(
+        default_factory=dict,
+        description="Speaker contacts (email, LinkedIn, etc.)"
+    )
